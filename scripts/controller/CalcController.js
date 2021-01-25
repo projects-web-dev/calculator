cabb1dfd52afde8e38d4a234b931f5362346487d
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-25 15:11:08 -0300

    (CalcController.js, at scrpits/controller/) -> acessing document elements in initialize method using document.querySelector(#<id>), for displayCalc, date and time ids wich refers to HTML tags (tags are objects in DOM)

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,6 +4,20 @@ class CalcController {
 	{
 		this._displayCalc = "0";
 		this._currentDate;
+		this.initialize();
+	}
+
+	initialize()
+	{
+		// El refers to HTML Element
+		let displayCalcEl = document.querySelector(
+			"#display");
+		let dateEl = document.querySelector("#data");
+		let timeEl = document.querySelector("#hora");
+
+		displayCalcEl.innerHTML = "4567";
+		dateEl.innerHTML = "25/01/2021"
+		timeEl.innerHTML = "00:00"
 	}
 
 	get displayCalc()

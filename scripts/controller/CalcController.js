5b73a2056616565ca2c1b0ac1b591facbbf19d9c
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-02-01 22:14:28 -0300

    (calculator.js and controller/CalcController.js, at scripts/) -> [10. C05 - Data e Hora] refresh date and time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,37 +2,75 @@ class CalcController {
 
 	constructor()
 	{
-		this._displayCalc = "0";
+		
+		this._locale = 'pt-Br';
+
+		// El refers to HTML Element
+		this._displayCalcEl = document.querySelector(
+			"#display");
+		this._dateEl = document.querySelector("#date");
+		this._timeEl = document.querySelector("#time");
+		
 		this._currentDate;
 		this.initialize();
 	}
 
-	initialize()
-	{
-		// El refers to HTML Element
-		let displayCalcEl = document.querySelector(
-			"#display");
-		let dateEl = document.querySelector("#date");
-		let timeEl = document.querySelector("#time");
+	initialize(){
 
-		displayCalcEl.innerHTML = "4567";
-		dateEl.innerHTML = "25/01/2021"
-		timeEl.innerHTML = "00:00"
+		this.setDisplayDateTime();
+
+		let interval = setInterval(()=>{
+
+			this.setDisplayDateTime();
+
+		}, 1000);
+
+		// to stop refresh the displayDate and displayTime after 10 seconds after the interval
+		// have been initialized
+
+		// setTimeout(()=>{
+
+		// 	clearInterval(interval);
+
+		// }, 10000);
+	}
+
+	setDisplayDateTime(){
+
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+	}
+
+	get displayTime(){
+		return this._timeEl.innerHTML;
+	}
+
+	set displayTime(value){
+		this._timeEl.innerHTML = value;
+	}
+
+	get displayDate(){
+		return this._dateEl.innerHTML;
+	}
+
+	set displayDate(value){
+		this._dateEl.innerHTML = value;
 	}
 
 	get displayCalc()
 	{
-		return this._displayCalc;
+		return this._displayCalcEl.innerHTML;
 	}
 
 	set displayCalc(value)
 	{
-		this._displayCalc = value;
+		this._displayCalcEl.innerHTML = value;
 	}
 
 	get currentDate()
 	{
-		return this._currentDate;
+		return new Date();
 	}
 
 	set currentDate(value)

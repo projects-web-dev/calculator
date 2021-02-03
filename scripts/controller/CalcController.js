8de40b869bc64809b11beaa5e4b1b329ba8011b2
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-02-03 18:40:17 -0300

    (calculator.js and controller/CalcController.js, at scripts/) -> [14. C09 - Switch] Using switch to get the select user click button [ERROR] solve all cases falling in default

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,7 +2,7 @@ class CalcController {
 
 	constructor()
 	{
-		
+		this._operation = [];
 		this._locale = 'pt-Br';
 
 		// El refers to HTML Element
@@ -46,6 +46,76 @@ class CalcController {
 		});
 	}
 
+
+
+	clearAll(){
+		this._operation = [];
+	}
+
+	clearEntry(){
+		this._operation.pop();
+	}
+
+	addOperation(value){
+
+		this._operation.push(value);
+
+		console.log(this._operation);
+	}
+
+	setError(){
+		this.displayCalc = "Error";
+	}
+
+	execBtn(value){
+
+		switch (value) {
+
+			case 'ac':
+				this.clearAll();
+				break;
+
+			case 'ce':
+				this.clearEntry();
+				break;
+
+			case 'soma':
+				break;
+
+			case 'subtracao':
+				break;
+
+			case 'divisao':
+				break;
+
+			case 'multiplicacao':
+				break;
+
+			case 'porcento':
+				break;
+
+			case 'igual':
+				break;
+
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				this.addOperation(parseInt(value));
+				break;
+
+			default:
+				this.setError();
+				break;
+		}
+	}
+
 	initButtonsEvents(){
 
 		// get the first g tag which are children of #button id
@@ -65,7 +135,11 @@ class CalcController {
 				// console.log(btn); // display g element (button) in the console
 				// console.log(btn.className.baseVal); // getting the class of the button (g element)
 				// console.log(index); // display the index of the button (g element)
-				console.log(btn.className.baseVal.replace("btn-", "")); // getting only what comes after "btn-" in class name of the btn (g element)
+				// console.log(btn.className.baseVal.replace("btn-", "")); // getting only what comes after "btn-" in class name of the btn (g element)
+
+				let textBtn = btn.className.baseVal.replace("btn-", "");
+
+				this.execBtn();
 
 			});
 
